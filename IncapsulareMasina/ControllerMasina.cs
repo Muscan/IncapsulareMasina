4c75764387af4884b3d799a87a450b72781078be
Mihai <kubikweg@gmail.com>
2021-08-31 16:26:48 +0300

    Feature: added CRUD

--- IncapsulareMasina/ControllerMasina.cs
+++ IncapsulareMasina/ControllerMasina.cs
@@ -18,16 +18,17 @@ namespace IncapsulareMasina
             //10 masini
 
 
-            Masina masina1 = new Masina("Audi", true, 1400);//declarare obiecte
-            Masina masina2 = new Masina("VW", true, '5', 4000);
-            Masina masina3 = new Masina("Suzuki", false, true, '1', 1000);
-            Masina masina4 = new Masina("Mazda", true, true, '9', 15000);
-            Masina masina5 = new Masina("Atelaj");
-            Masina masina6 = new Masina("Tir", true, 100000);
-            Masina masina7 = new Masina("Tico");
-            Masina masina8 = new Masina("Mercedes", true, '5', 50000);
-            Masina masina9 = new Masina("SKoda", true, 1000);
+            Masina masina1  = new Masina("Audi", true, 1400);//declarare obiecte
+            Masina masina2  = new Masina("VW", true, '5', 4000);
+            Masina masina3  = new Masina("Suzuki", false, true, '1', 1000);
+            Masina masina4  = new Masina("Mazda", true, true, '9', 15000);
+            Masina masina5  = new Masina("Atelaj");
+            Masina masina6  = new Masina("Tir", true, 100000);
+            Masina masina7  = new Masina("Tico");
+            Masina masina8  = new Masina("Mercedes", true, '5', 50000);
+            Masina masina9  = new Masina("SKoda", true, 1000);
             Masina masina10 = new Masina("opel", true, '4', 5000);
+        
 
    
 
@@ -57,9 +58,6 @@ namespace IncapsulareMasina
         public Masina  pretMinim()
         {
             Masina minima = masini[0];
-
-
-           
 
             for (int i = 0; i < masini.Count; i++)
             {
@@ -116,6 +114,100 @@ namespace IncapsulareMasina
             return -1;
         }
 
+
+        //todo o functie ce primeste ca parametru o masina si rretuneaza true daca o dauga sau false daca nu see poate adauga
+
+
+        public bool AddCar(Masina masina)
+        {
+
+            int poz = pozitieMasina(masina.getNume());
+
+            if (poz == -1)
+            {
+
+                this.masini.Add(masina);
+                return true;
+            }  
+            return false;
+        }
+
+        public bool UpdateNumeCar(string nume, double pret)
+        {
+
+            int p = pozitieMasina(nume);
+
+            if (p != -1)
+            {
+                masini[p].setPret(pret);
+                return true;
+            }
+            return false;
+
+        }
+     
+        public bool UpdateTransmisieAutomaticMasina(string nume, bool IsAutomatic)
+        {
+            int p = pozitieMasina(nume);
+            if (p != -1)
+            {
+                masini[p].setAutomatic(false);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateTransmisieManual(string nume, bool manual)
+        {
+            int p = pozitieMasina(nume);
+            if (p != -1)
+            {
+                masini[p].setManual(false);
+                return true;
+            }
+            return false;
+        }
+
+        public char UpdateNcapMasina(string nume, char ncap)
+        {
+            char c = ' ';
+            int p = pozitieMasina(nume);
+            if (p != -1)
+            {
+                masini[p].setNcap(ncap);
+                c = ncap;
+            }
+            return ncap;
+        }
+
+        public double UpdatePret(string nume, double pret)
+        {
+            double pretNou = 0;
+            int p = pozitieMasina(nume);
+            
+            if (p != -1)
+            {
+                masini[p].setPret(pret);
+                pretNou = pret;
+            }
+            return pret;
+
+        }
+
+        //delete car.Marca-> sterg masina Nume
+        public bool DeleteCar(string masina)
+        {
+
+            int p = pozitieMasina(masina);
+            if (p != -1)
+            {
+                this.masini.Remove(masini[p]);
+                return true;
+
+            }
+            return false;
+          
+        }
 
     }
 }
--- IncapsulareMasina/Program.cs
+++ IncapsulareMasina/Program.cs
@@ -12,20 +12,45 @@ namespace IncapsulareMasina
 
 
 
-            Masina m = control.pretMinim();
-           
+            //control.afisare();
 
-            //Console.WriteLine(m.descriereMasina());
-            
-            Masina max = control.pretMaxim();
 
-            //Console.WriteLine(max.descriereMasina());
+            Masina m = new Masina("test",true, true, '2', 1200);
 
-            control.masiniAutomate();
-      
+            Console.WriteLine(control.AddCar(m));
+
+
+
+            //Console.WriteLine(control.UpdateCar("test", 23333));
+            //control.UpdateNumeCar("test", 1000);
+           // Console.WriteLine("Masina updatata:" + m.descriereMasina());
+
+            //control.UpdateTransmisieAutomaticMasina("test", false);
+           // Console.WriteLine("Masina cu transmisie updatata de la: "+ m.descriereMasina());
+
+            //control.UpdateTransmisieManual("test", false);
+            //Console.WriteLine("Masina cu manuala" + m.descriereMasina());
+
+            //control.UpdateNcapMasina("test", '9');
+            //Console.WriteLine("Masina cu ncap NOU "+ m.descriereMasina());
+
+            //control.UpdatePret("test", 999999);
+            //Console.WriteLine("Pret nou"+"\n"+ m.descriereMasina());
+
+            //Delete
           
-            
-            
+
+            Masina car = new Masina("test1", true, true, '2', 19006);
+            Console.WriteLine(control.DeleteCar("test1"));
+            control.DeleteCar("test");
+            //tbd
+            Console.WriteLine("Masina stearsa" + "\n" + m.descriereMasina());
+
+
+
+
+
+
         }
     }
 }
